852db0ca8c81662d7a5f4cae9c3faf3aee458615
GuardME <ramadanyosi99@gmail.com>
2024-03-10 05:06:24 +0700

    feat(create-new-teams): integration with convex

---
+++ src/app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex } from "convex/react";
+import { useRouter } from "next/navigation";
+import { api } from "../../../../convex/_generated/api";
+
+function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const convex = useConvex();
+  const { user }: any = useKindeBrowserClient();
+  const [fileList_, setFileList_] = useState();
+  const router = useRouter();
+  useEffect(() => {
+    user && checkTeam();
+  }, [user]);
+
+  const checkTeam = async () => {
+    const result = await convex.query(api.teams.getTeam, {
+      email: user?.email,
+    });
+
+    if (!result?.length) {
+      router.push("teams/create");
+    }
+  };
+  return <div>{children}</div>;
+}
+
+export default DashboardLayout;
