9cea453799fdae6f8015a40f381a0f39d06e7ff1
GuardME <ramadanyosi99@gmail.com>
2024-03-25 17:49:54 +0700

    feat(canvas): feature canvas

--- src/app/(routes)/workspace/[field]/page.tsx
+++ src/app/(routes)/workspace/[field]/page.tsx
@@ -2,17 +2,26 @@
 import React, { useEffect, useState } from "react";
 import WorkSpaceHeader from "../_components/WorkspaceHeader";
 import Editor from "../_components/Editor";
+import Canvas from "../_components/Canvas";
 import { useConvex } from "convex/react";
 import { FILE } from "../../dashboard/_components/FileList";
+import { api } from "../../../../../convex/_generated/api";
 
 function Workspace({ params }: any) {
   const [triggerSave, setTriggerSave] = useState(false);
   const convex = useConvex();
   const [fileData, setFileData] = useState<FILE | any>();
   useEffect(() => {
-    console.log("FILEID",params)
-    params.field;
+    console.log("FILEID", params);
+    params.field && getFileData();
   }, []);
+
+  const getFileData = async () => {
+    const result = await convex.query(api.files.getFileById, {
+      _id: params.field,
+    });
+    setFileData(result);
+  };
 
   return (
     <div>
@@ -29,7 +38,13 @@ function Workspace({ params }: any) {
           />
         </div>
         {/* Whiteboard Canvas */}
-        <div className="bg-red-500 h-screen">Canvas</div>
+        <div className="h-screen border-l">
+          <Canvas
+            onSaveTrigger={triggerSave}
+            fileId={params.field}
+            fileData={fileData}
+          />
+        </div>
       </div>
     </div>
   );
